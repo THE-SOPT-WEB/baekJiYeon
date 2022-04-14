3758e67bafc07a169325e2126887f47d47ee6279
100Gyeon <bjy9966@naver.com>
2022-04-15 02:21:30 +0900

    feat: 정답이면 다음 단계로 넘어가고, 오답이면 틀렸다는 모달 띄우기

--- week2-1/main.js
+++ week2-1/main.js
@@ -30,3 +30,68 @@ const quizList = [
     answer: "백지연",
   },
 ];
+
+function showModal(modalContent, keepOpen) {
+  const modal = $(".modal");
+  const modalBody = $("p.modal__body");
+
+  modalBody.innerHTML = modalContent;
+  modal.classList.remove("hide");
+
+  if (keepOpen) return;
+
+  if (currentStep !== quizList.length) {
+    setTimeout(() => {
+      modal.classList.add("hide");
+    }, 500);
+  }
+}
+
+function goNextStep(image) {
+  currentStep++;
+
+  // 게임이 끝난 상태
+  if (currentStep === quizList.length) {
+    showModal(`<a href="/">메인화면으로</a>`);
+    return;
+  }
+
+  image.src = quizList[currentStep].src;
+}
+
+function attachEvent({ answer, image }) {
+  answer.addEventListener("click", (e) => {
+    if (e.target instanceof HTMLElement) {
+      const userAnswer = e.target.innerText;
+      const realAnswer = quizList[currentStep].answer;
+      if (userAnswer == realAnswer) {
+        showModal("나를 알아주다니 고마워💜");
+        setTimeout(() => {
+          goNextStep(image);
+        }, 500);
+      } else {
+        showModal(`틀렸어 나는 ${userAnswer} 아니야 !! 🤬`);
+      }
+    }
+  });
+}
+
+function initGame(score, image) {
+  currentStep = 0;
+  score.innerText = 0;
+  image.src = quizList[currentStep].src;
+}
+
+function gameManager(gameInfo) {
+  const { score, image } = gameInfo;
+  initGame(score, image);
+  attachEvent(gameInfo);
+}
+
+window.onload = () => {
+  gameManager({
+    score: $(".scoreBoard__score"),
+    answer: $("ul.answer__list"),
+    image: $(".imageBoard > img"),
+  });
+};
