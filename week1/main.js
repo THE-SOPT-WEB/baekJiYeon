2f3adbde861be8f4367e34211a04cc888123bbb2
100Gyeon <bjy9966@naver.com>
2022-04-15 23:59:39 +0900

    refactor: 누적금액 계산하는 부분 리팩토링, 변수명 수정

--- week1/main.js
+++ week1/main.js
@@ -1,7 +1,7 @@
 const $ = (selector) => document.querySelector(selector);
 const $All = (selector) => document.querySelectorAll(selector);
 
-const burgerCard = $All(".burger__card");
+const burgerCardList = $All(".burger__card");
 const cartList = $(".cart__list");
 const orderBtn = $(".cart__order-btn");
 const cancelBtn = $(".cart__cancel-btn");
@@ -16,27 +16,25 @@ const parsePriceToNumber = (price) => {
 };
 
 // 누적 금액 변동
-const changeSumText = (type, price) => {
-  switch (type) {
-    case "plus":
-      sum += parsePriceToNumber(price);
-      break;
-    case "minus":
-      sum -= parsePriceToNumber(price);
-      break;
-    case "reset":
-      sum = price;
-      break;
-  }
-  $(".cart__sum span").innerText = sum ? `${sum.toLocaleString()}원` : "0원";
+const changeSum = () => {
+  const cartItemList = $All(".cart__item");
+  const priceList = [];
+  cartItemList.forEach((item) => {
+    const price = parsePriceToNumber(
+      item.querySelector(".item__price").innerText
+    );
+    priceList.push(price);
+  });
+  const sum = priceList.reduce((acc, cur) => acc + cur, 0);
+  $(".cart__sum span").innerText = `${sum.toLocaleString()}원`;
 };
 
-burgerCard.forEach((card) => {
+burgerCardList.forEach((card) => {
   card.addEventListener("click", (e) => {
     const burgerName = card.querySelector(".burger__name").innerText;
     const burgerPrice = card.querySelector(".burger__price").innerText;
     addBurger(burgerName, burgerPrice);
-    changeSumText("plus", burgerPrice);
+    changeSum();
   });
 });
 
@@ -59,42 +57,41 @@ cancelBtn.addEventListener("click", () => {
   while (cartList.hasChildNodes()) {
     cartList.removeChild(cartList.firstChild);
   }
-  changeSumText("reset", 0);
+  changeSum();
 });
 
 // 장바구니에 태그 동적으로 삽입하고 삭제하기
 const addBurger = (burgerName, burgerPrice) => {
-  const li = document.createElement("li");
-  const name = document.createElement("span");
-  const count = document.createElement("input");
-  const price = document.createElement("span");
+  const cartItem = document.createElement("li");
+  const nameSpan = document.createElement("span");
+  const countInput = document.createElement("input");
+  const priceSpan = document.createElement("span");
   const deleteBtn = document.createElement("button");
 
-  li.setAttribute("class", "cart__item");
+  cartItem.setAttribute("class", "cart__item");
 
-  name.setAttribute("class", "item__name");
-  name.innerText = burgerName;
+  nameSpan.setAttribute("class", "item__name");
+  nameSpan.innerText = burgerName;
 
-  count.setAttribute("class", "item__count");
-  count.setAttribute("type", "number");
-  count.setAttribute("min", 1);
-  count.setAttribute("value", 1);
+  countInput.setAttribute("class", "item__count");
+  countInput.setAttribute("type", "number");
+  countInput.setAttribute("min", 1);
+  countInput.setAttribute("value", 1);
 
-  price.setAttribute("class", "item__price");
-  price.innerText = burgerPrice;
+  priceSpan.setAttribute("class", "item__price");
+  priceSpan.innerText = burgerPrice;
 
   deleteBtn.setAttribute("class", "item__delete-btn");
   deleteBtn.innerText = "X";
 
-  cartList.appendChild(li);
-  li.appendChild(name);
-  li.appendChild(count);
-  li.appendChild(price);
-  li.appendChild(deleteBtn);
+  cartList.appendChild(cartItem);
+  cartItem.appendChild(nameSpan);
+  cartItem.appendChild(countInput);
+  cartItem.appendChild(priceSpan);
+  cartItem.appendChild(deleteBtn);
 
   deleteBtn.addEventListener("click", () => {
-    const itemPrice = li.querySelector(".item__price").innerText;
-    li.remove();
-    changeSumText("minus", itemPrice);
+    cartItem.remove();
+    changeSum();
   });
 };
