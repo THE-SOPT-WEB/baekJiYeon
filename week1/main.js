9dbd2986e3f6bc681c94f7420b0cb65c66c2750a
Jiyeon Baek <58380158+100Gyeon@users.noreply.github.com>
2022-04-16 01:33:33 +0900

    feat: 이미 장바구니에 동일한 버거 있을 때 수량만 늘어나게 처리

--- week1/main.js
+++ week1/main.js
@@ -1,7 +1,7 @@
 const $ = (selector) => document.querySelector(selector);
 const $All = (selector) => document.querySelectorAll(selector);
 
-const burgerCardList = $All(".burger__card");
+const burgerCardNodeList = $All(".burger__card");
 const cartList = $(".cart__list");
 const orderBtn = $(".cart__order-btn");
 const cancelBtn = $(".cart__cancel-btn");
@@ -17,9 +17,9 @@ const parsePriceToNumber = (price) => {
 
 // 누적 금액 변동
 const changeSum = () => {
-  const cartItemList = $All(".cart__item");
+  const cartItemNodeList = $All(".cart__item");
   const priceList = [];
-  cartItemList.forEach((item) => {
+  cartItemNodeList.forEach((item) => {
     const price = parsePriceToNumber(
       item.querySelector(".item__price").innerText
     );
@@ -30,7 +30,61 @@ const changeSum = () => {
   $(".cart__sum span").innerText = `${sum.toLocaleString()}원`;
 };
 
-burgerCardList.forEach((card) => {
+// 장바구니에 태그 동적으로 삽입하고 삭제하기
+const addBurger = (burgerName, burgerPrice) => {
+  // 이미 장바구니에 동일한 버거 있을 때 수량만 늘어나게 처리
+  if (cartList.hasChildNodes()) {
+    const children = cartList.childNodes;
+    for (let i = 0; i < children.length; i++) {
+      if (burgerName === children[i].querySelector(".item__name").innerText) {
+        children[i].querySelector(".item__count").value++;
+        return;
+      }
+    }
+  }
+
+  const cartItem = document.createElement("li");
+  const nameSpan = document.createElement("span");
+  const countInput = document.createElement("input");
+  const priceSpan = document.createElement("span");
+  const deleteBtn = document.createElement("button");
+
+  cartItem.setAttribute("class", "cart__item");
+
+  nameSpan.setAttribute("class", "item__name");
+  nameSpan.innerText = burgerName;
+
+  countInput.setAttribute("class", "item__count");
+  countInput.setAttribute("type", "number");
+  countInput.setAttribute("min", 1);
+  countInput.setAttribute("value", 1);
+
+  priceSpan.setAttribute("class", "item__price");
+  priceSpan.innerText = burgerPrice;
+
+  deleteBtn.setAttribute("class", "item__delete-btn");
+  deleteBtn.innerText = "X";
+
+  cartList.appendChild(cartItem);
+  cartItem.appendChild(nameSpan);
+  cartItem.appendChild(countInput);
+  cartItem.appendChild(priceSpan);
+  cartItem.appendChild(deleteBtn);
+
+  // 수량 변경
+  countInput.addEventListener("change", () => {
+    changeSum();
+  });
+
+  // 특정 아이템 장바구니에서 빼기
+  deleteBtn.addEventListener("click", () => {
+    cartItem.remove();
+    changeSum();
+  });
+};
+
+// 버거 카드 누르면 장바구니에 추가
+burgerCardNodeList.forEach((card) => {
   card.addEventListener("click", (e) => {
     const burgerName = card.querySelector(".burger__name").innerText;
     const burgerPrice = card.querySelector(".burger__price").innerText;
@@ -60,43 +114,3 @@ cancelBtn.addEventListener("click", () => {
   }
   changeSum();
 });
-
-// 장바구니에 태그 동적으로 삽입하고 삭제하기
-const addBurger = (burgerName, burgerPrice) => {
-  const cartItem = document.createElement("li");
-  const nameSpan = document.createElement("span");
-  const countInput = document.createElement("input");
-  const priceSpan = document.createElement("span");
-  const deleteBtn = document.createElement("button");
-
-  cartItem.setAttribute("class", "cart__item");
-
-  nameSpan.setAttribute("class", "item__name");
-  nameSpan.innerText = burgerName;
-
-  countInput.setAttribute("class", "item__count");
-  countInput.setAttribute("type", "number");
-  countInput.setAttribute("min", 1);
-  countInput.setAttribute("value", 1);
-
-  priceSpan.setAttribute("class", "item__price");
-  priceSpan.innerText = burgerPrice;
-
-  deleteBtn.setAttribute("class", "item__delete-btn");
-  deleteBtn.innerText = "X";
-
-  cartList.appendChild(cartItem);
-  cartItem.appendChild(nameSpan);
-  cartItem.appendChild(countInput);
-  cartItem.appendChild(priceSpan);
-  cartItem.appendChild(deleteBtn);
-
-  countInput.addEventListener("change", () => {
-    changeSum();
-  });
-
-  deleteBtn.addEventListener("click", () => {
-    cartItem.remove();
-    changeSum();
-  });
-};
