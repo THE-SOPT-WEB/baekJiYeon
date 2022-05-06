bf40fc4e918542c7dea988ebfaef4a310d983949
100Gyeon <bjy9966@naver.com>
2022-05-06 17:50:59 +0900

    chore: 프리티어 설정 수정, 이미지 정리

--- week3/src/components/common/Router.js
+++ week3/src/components/common/Router.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./ScrollToTop";
-import Home from "pages/Home";
-import Game from "pages/Game";
-import Result from "pages/Result";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
+import Home from 'pages/Home';
+import Game from 'pages/Game';
+import Result from 'pages/Result';
 
 function Router() {
   return (
--- week3/src/pages/Home.jsx
+++ week3/src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import { imgHome } from "assets";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { imgHome } from 'assets';
 
 function Home() {
   return (
